8c81ac62c379c17d925e651944c3f285b55f832e
Roberto Carneiro <pc.roberto2@gmail.com>
2019-06-04 15:48:19 -0300

    Corrigido erro de grafia no corpo do e-mail de notificação

--- resources/views/emails/radar.blade.php
+++ resources/views/emails/radar.blade.php
@@ -2,7 +2,7 @@
     <body>
         <p>Olá, {{ $radar->user->name }}!</p>
         <p></p>
-        <p>Este e-mail tem o objetivo de lhe informar que houve um(a) <b>{{$ocorrencia->crime->descricao}}</b> na região monitorada pelos seu radar <b>{{$radar->nome}}<b></p>
+        <p>Este e-mail tem o objetivo de lhe informar que houve um(a) <b>{{$ocorrencia->crime->descricao}}</b> na região monitorada pelo seu radar <b>{{$radar->nome}}<b></p>
         <p>O fato ocorreu em {{ \Carbon\Carbon::create($ocorrencia->datahora)->format('d/m/Y') }} às {{ \Carbon\Carbon::create($ocorrencia->datahora)->format('H:i') }}.</p>
         <p>O endereço da ocorrência é: {{ $ocorrencia->localizacao->endereco }}</p>
         <p>Fique atento!</p>
